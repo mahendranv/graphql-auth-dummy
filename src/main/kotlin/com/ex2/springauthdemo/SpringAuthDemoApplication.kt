ecc8de05bf8d3ceee0163472eab09eb6362fc04f
Mahendran Vadivalagan <mahendran.vadivalagan@gmail.com>
2021-05-31 23:11:17 +0530

    Initial commit

---
+++ src/main/kotlin/com/ex2/springauthdemo/SpringAuthDemoApplication.kt
@@ -0,0 +1,11 @@
+package com.ex2.springauthdemo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringAuthDemoApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringAuthDemoApplication>(*args)
+}
---
+++ src/test/kotlin/com/ex2/springauthdemo/SpringAuthDemoApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.ex2.springauthdemo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringAuthDemoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
